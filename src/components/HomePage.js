765911708ebcd1c05da86c5da01f238927d2fd6c
Caro Evans <caroline.w.evans@gmail.com>
2018-07-11 16:44:36 +1000

    styling home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HomePage = () => (
     <div className="Homepage">
             <p className="Homepage--nametitle"> Michael Waye </p>
-            <p className="Homepage--subtitle"> musical production </p>
-            <p className="Homepage--slogan"> Proffessional Producer assistance </p>
+            <p className="Homepage--subtitle"> MUSICAL PRODUCTION </p>
+            <p className="Homepage--slogan"> Professional Producer assistance </p>
             <p className="Homepage--slogan"> Consultancy, Producing Quality Music & Technical Help </p>
             <div className="Homepage--icons">
             <img className="Homepage--icon" src="http://icons.iconarchive.com/icons/xenatt/minimalism/256/App-SoundCloud-icon.png" alt="soundcload logo"/>
